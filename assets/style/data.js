f8a517c6fd5d3c0c6b2ad3e3c8ed6ebba4e68017
Louise Wong <lw17122@bristol.ac.uk>
2021-11-17 20:34:14 +0000

    added data.js for questions + changed options method

---
+++ assets/style/data.js
@@ -0,0 +1,52 @@
+export const questions = [
+  {
+    question: "What colour shade was used by Nasa for its Space Shuttle suits?",
+    optionA: "Worldwide White",
+    optionB: "Black Hole Blue",
+    optionC: "International Orange",
+    optionD: "Royal Blue",
+    correctOption: "International Orange"},
+    // options: ["Worldwide White","Black Hole Blue", "International Orange", "Royal Blue"],
+    // correctOption: "International Orange"},
+
+{
+  question: "How long after the execution of Anne Boleyn did Henry VIII marry Jane Seymour?",
+  optionA: "11 days",
+  optionB: "21 days",
+  optionC: "11 months",
+  optionD: "11 years",
+  correctOption: "11 days"},
+  // options: ["11 days", "21 days", "11 months", "11 years"],
+  // correctOption: "11 days"},
+
+{
+  question: "British Summer Time begins on the last Sunday in which month?",
+  optionA: "February",
+  optionB: "March",
+  optionC: "April",
+  optionD: "May",
+  correctOption: "March"}
+  // options: ["February", "March", "April", "May"],
+  // correctOption: "March"}
+
+// {
+//   question: "What does the Welsh word 'sglodion' mean in English?",
+//   options: ["Chips", "Tea", "Cardigan", "Coins"],
+//   correctOption: "Chips"},
+
+// {
+//   question: "Street artist Banksy is originally associated with which British city?",
+//   options: ["London", "Sheffield", "Bristol", "Bath"],
+//   correctOption: "Bristol"},
+
+// {
+//   question: "What is the capital of New Zealand?",
+//   options: ["Auckland", "Hamilton", "Queenstown", "Wellington"],
+//   correctOption: "Wellington"},
+
+// {
+//   question: "Which London Underground line has the most stations?",
+//   options: ["Northern Line", "Central Line", "Jubilee Line", "District Line"],
+//   correctOption: "District Line"},
+
+]
